8e92b6d36972f70d8dcfc7175fee38a6364b8f82
Isabelle Sanford <isabellesanford@gmail.com>
2021-01-12 14:41:05 -0800

    alignment pie v1 complete

--- static/js/players.js
+++ static/js/players.js
@@ -1,9 +1,10 @@
 // horizontal bar chart
-function drawPiePlot(values, labels, div) {
+function drawPiePlot(values, labels, colors, div) {
     let data = [{
         values: values,
         labels: labels,
-        type: 'pie'
+        type: 'pie',
+        marker: {colors: colors}
       }];
       
 
@@ -15,12 +16,14 @@ function drawPiePlot(values, labels, div) {
 };
 
 function init() {
-    d3.json('./datajsons/playerdata.json').then(function(data) {
+    d3.json('https://isabelle-sanford.github.io/se-data-analysis/datajsons/playerdata.json').then(function(data) {
 
-        var players = data[0];
 
-        alignment_list = players.map(d => d.alignment);
-        var unique_aligns = [... new Set(alignment_list)];
+
+        alignment_list = data.map(d => d.alignment);
+        var unique_aligns = ["G", 'E', 'M', 'N', 'F', 'S', 'T', 'B', 'D', 'C'];
+        var align_labels = ['Good', 'Evil', 'SK', 'Neutral', 'Faction', 'Other Evil']
+        var align_colors = ['green', 'red', 'orange', 'gray', 'purple', 'pink']
 
         var align_cts = []
 
@@ -30,9 +33,20 @@ function init() {
 
         })
 
+        var c_ct = align_cts.pop();
+        var d_ct = align_cts.pop();
+        var b_ct = align_cts.pop();
+        var t_ct = align_cts.pop();
+        var s_ct = align_cts.pop();
+
+        var other_evils = c_ct + d_ct + b_ct + t_ct + s_ct;
+
+        align_cts.push(other_evils);
+
+        console.log(align_cts.length);
 
 
-        drawPiePlot(align_cts, unique_aligns, 'alignment-pie');
+        drawPiePlot(align_cts, align_labels, align_colors, 'alignment-pie');
 
     });
 
